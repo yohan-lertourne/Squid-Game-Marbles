d0c9dbc398e09820f9e360804a11866f716beac4
MatthieuBulcke <mattdu59890@gmail.com>
2021-12-16 12:08:15 +0100

    C le push

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ function initPlayers() {
 let figlist = document.getElementsByTagName("figure");
 let figcaptionlist = document.getElementsByTagName("figcaption");
 let varStor = "0";
-for (let i = 0; i < figcaptionlist.length; i++) {
-    figlist[i].addEventListener("click", function () {
-        window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML);
-        document.write(varStor);
-    });
-}
+initPlayers();
+document.addEventListener('DOMContentLoaded', function() {
+    for(let i = 0; i < figcaptionlist.length; i++){
+        figlist[i].addEventListener("click",function(){
+            window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML); 
+            document.write(window.localStorage.getItem("nombreBilles"))
+    })}
+});
--- main.ts
+++ main.ts
@@ -3,13 +3,15 @@ function initPlayers(){
     window.localStorage.setItem('nombreBillesJoueur1', billes);
     window.localStorage.setItem('nombreBillesJoueur2', billes);
 }
-
+initPlayers;
 let figlist = document.getElementsByTagName("figure");
 let figcaptionlist = document.getElementsByTagName("figcaption");
 
 let varStor : string ="0";
-for(let i = 0; i < figcaptionlist.length; i++){
-    figlist[i].addEventListener("click",function(){
-        window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML); 
-        document.write(varStor)
-})}
+document.addEventListener('DOMContentLoaded', function() {
+    for(let i = 0; i < figcaptionlist.length; i++){
+        figlist[i].addEventListener("click",function(){
+            window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML); 
+            document.write(varStor)
+    })}
+});
