5a502d3a7bc663370372cffad0637ca200a6623c
yohan-lertourne <yohan.lertourne@gmail.com>
2021-12-16 09:22:44 +0100

    Create j2.js
    function pair and impair

---
+++ j2.js
@@ -0,0 +1,18 @@
+let number;
+function pair (number) {
+    if (number % 2 == 0){
+        return ("win");
+    }
+    else {
+        return ("lose");
+    }
+}
+
+function impair (number){
+    if (number % 2 == 0){
+        return ("lose");
+    }
+    else {
+        return ("win");
+    }
+}
