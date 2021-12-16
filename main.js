53285adc78f4b456ec8ef8fb77cb96060b563d7b
Rodrigue Ulua <71713144+rodrigueulua@users.noreply.github.com>
2021-12-16 12:50:56 +0100

    main corrigé

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ initPlayers();
 let figlist = document.getElementsByTagName("figure");
 let figcaptionlist = document.getElementsByTagName("figcaption");
 let varStor = 0;
-for (let i = 0; i < figcaptionlist.length; i++) {
-    figlist[i].addEventListener("click", function () {
-        window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML);
-        varStor = window.localStorage.getItem("nombreBilles");
-        document.write(varStor);
-    });
-}
+document.addEventListener('DOMContentLoaded', function () {
+    for (let i = 0; i < figcaptionlist.length; i++) {
+        figlist[i].addEventListener("click", function () {
+            window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML);
+            varStor = window.localStorage.getItem("nombreBilles");
+            document.write(varStor);
+        });
+    }
+});
--- main.ts
+++ main.ts
@@ -8,9 +8,11 @@ let figlist = document.getElementsByTagName("figure");
 let figcaptionlist = document.getElementsByTagName("figcaption");
 
 let varStor : any = 0;
-for(let i = 0; i < figcaptionlist.length; i++){
-    figlist[i].addEventListener("click",function(){
-        window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML);
-        varStor = window.localStorage.getItem("nombreBilles");
-        document.write(varStor)
-})}
+document.addEventListener('DOMContentLoaded', function() {
+    for(let i = 0; i < figcaptionlist.length; i++){
+        figlist[i].addEventListener("click",function(){
+            window.localStorage.setItem('nombreBilles', figcaptionlist[i].innerHTML);
+            varStor = window.localStorage.getItem("nombreBilles")
+            document.write(varStor)
+    })}
+});
