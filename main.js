8a4d7694dbc982815daa66ac4a0504e032e88452
Rodrigue Ulua <71713144+rodrigueulua@users.noreply.github.com>
2021-12-16 15:08:56 +0100

    reverse player

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 "use strict";
+let sectionInverse = document.getElementsByTagName("section");
+let titreJoueurs = document.getElementsByTagName("h2");
+let varStor = 0;
+let figlist = document.getElementsByTagName("figure");
+let figcaptionlist = document.getElementsByTagName("figcaption");
 function initPlayers() {
     let billes = 10;
     window.localStorage.setItem('nombreBillesJoueur1', billes);
     window.localStorage.setItem('nombreBillesJoueur2', billes);
 }
 initPlayers();
-let figlist = document.getElementsByTagName("figure");
-let figcaptionlist = document.getElementsByTagName("figcaption");
-let varStor = 0;
 document.addEventListener('DOMContentLoaded', function () {
     for (let i = 0; i < figcaptionlist.length; i++) {
         figlist[i].addEventListener("click", function () {
@@ -16,4 +18,16 @@ document.addEventListener('DOMContentLoaded', function () {
             document.write(varStor);
         });
     }
+});
+window.addEventListener("click", function () {
+    sectionInverse[0].classList.toggle("reverse");
+    if (titreJoueurs[0].innerHTML == "Joueur 1") {
+        titreJoueurs[0].innerHTML = "Joueur 2";
+        titreJoueurs[1].innerHTML = "Joueur 1";
+    }
+    else {
+        titreJoueurs[0].innerHTML = "Joueur 1";
+        titreJoueurs[1].innerHTML = "Joueur 2";
+    }
+    console.log(titreJoueurs[0].textContent);
 });
