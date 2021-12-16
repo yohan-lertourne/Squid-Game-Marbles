c21d4fdd655675427b2cf559808ccadf24d2ca01
yohan-lertourne <yohan.lertourne@gmail.com>
2021-12-16 14:58:31 +0100

    over + calcul

--- j2.js
+++ j2.js
@@ -3,20 +3,61 @@ let joueur1 = localStorage.getItem("nombreBillesJoueur1");
 let joueur2 = localStorage.getItem("nombreBillesJoueur2");
 let number = localStorage.getItem("nombreBilles");
 let get = document.getElementsByTagName("button");
+let actif = document.getElementsByTagName("h2");
+let test = actif[1].textContent;
+console.log(test);
+let joueur3 = Number(joueur1);
+let joueur4 = Number(joueur2);
+let number2 = Number(number);
+function win() {
+    if (test == "Joueur 1") {
+        joueur3 += number2;
+        joueur4 -= number2;
+        console.log(joueur3);
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    else if (test == "Joueur 2") {
+        joueur3 -= number2;
+        joueur4 += number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    if (joueur3 <= 0 || joueur4 <= 0) {
+        alert("over");
+    }
+}
+function lose() {
+    if (test == "Joueur 1") {
+        joueur3 -= number2;
+        joueur4 += number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    else if (test == "Joueur 2") {
+        joueur3 += number2;
+        joueur4 -= number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    if (joueur3 <= 0 || joueur4 <= 0) {
+        alert("over");
+    }
+}
 function pair() {
     if (number % 2 === 0) {
-        alert("win");
+        win();
     }
     else {
-        alert("lose");
+        lose();
     }
 }
 function impair() {
     if (number % 2 === 0) {
-        alert("lose");
+        lose();
     }
     else {
-        alert("win");
+        win();
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
--- j2.ts
+++ j2.ts
@@ -1,24 +1,69 @@
 let joueur1: any = localStorage.getItem("nombreBillesJoueur1");
 let joueur2: any = localStorage.getItem("nombreBillesJoueur2");
 let number : any = localStorage.getItem("nombreBilles");
-let get = document.getElementsByTagName("button");
+let get : any = document.getElementsByTagName("button");
+let actif: any = document.getElementsByTagName("h2");
+let test:string = actif[1].textContent;
+console.log(test);
+let joueur3= Number(joueur1);
+let joueur4= Number(joueur2);
+let number2= Number(number);
+function win() {
+    if (test == "Joueur 1") {
+        joueur3 += number2;
+        joueur4 -= number2;
+        console.log(joueur3);
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    else if (test == "Joueur 2") {
+        joueur3 -= number2;
+        joueur4 += number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    if (joueur3 <= 0 || joueur4 <= 0) {
+        alert("over");
+    }
+}
+
+function lose() {
+    if (test == "Joueur 1") {
+        joueur3 -= number2;
+        joueur4 += number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    else if (test == "Joueur 2") {
+        joueur3 += number2;
+        joueur4 -= number2;
+        localStorage.setItem("nombreBillesJoueur1", joueur3.toString());
+        localStorage.setItem("nombreBillesJoueur2", joueur4.toString());
+    }
+    if (joueur3 <= 0 || joueur4 <= 0) {
+        alert("over");
+    }
+}
+
 function pair () {
     if (number % 2 === 0){
-        alert("win");
+        win();
     }
     else {
-        alert("lose");
+        lose();
     }
 }
 
 function impair (){
     if (number % 2 === 0){
-        alert("lose");
+        lose();
     }
     else {
-        alert("win");
+        win();
     }
 }
+
+
 document.addEventListener('DOMContentLoaded', function() {
     get[0].addEventListener("click",pair);
     get[1].addEventListener("click",impair);
