df073bdbfed4e56650898f4a00c0a1a84d32103b
yohan-lertourne <yohan.lertourne@gmail.com>
2021-12-16 11:15:15 +0100

    update as ts

--- j2.js
+++ j2.js
@@ -1,23 +1,23 @@
+"use strict";
 let number = 2;
 let get = document.getElementsByTagName("button");
-function pair () {
-    if (number % 2 === 0){
+function pair() {
+    if (number % 2 === 0) {
         alert("win");
     }
     else {
         alert("lose");
     }
 }
-
-function impair (){
-    if (number % 2 === 0){
+function impair() {
+    if (number % 2 === 0) {
         alert("lose");
     }
     else {
         alert("win");
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
-    get[0].addEventListener("click",pair);
-    get[1].addEventListener("click",impair);
+document.addEventListener('DOMContentLoaded', function () {
+    get[0].addEventListener("click", pair);
+    get[1].addEventListener("click", impair);
 });
---
+++ j2.ts
@@ -0,0 +1,22 @@
+let number = 2;
+let get = document.getElementsByTagName("button");
+function pair () {
+    if (number % 2 === 0){
+        alert("win");
+    }
+    else {
+        alert("lose");
+    }
+}
+function impair (){
+    if (number % 2 === 0){
+        alert("lose");
+    }
+    else {
+        alert("win");
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    get[0].addEventListener("click",pair);
+    get[1].addEventListener("click",impair);
+});
